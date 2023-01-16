1b145896b7319712f844b851c7f431d12234a7be
savir <sh6147782@gmail.com>
2023-01-16 11:33:35 +0530

    implemented ExtentReports and failedTestRerunner

--- src/test/java/cucumberOptions/FailedTestRunner.java
+++ src/test/java/cucumberOptions/FailedTestRunner.java
@@ -5,10 +5,10 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, tags = "@validateCart or @serachProduct", plugin = {
+@CucumberOptions(features = "@target/failed_scenarios.txt", glue = "stepDefinitions", monochrome = true, plugin = {
 		"html:target/cucumber.html", "json:target/cucumber.jason",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "rerun:target/failed_scenarios.txt" })
-public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
+public class FailedTestRunner extends AbstractTestNGCucumberTests {
 
 	@Override
 	@DataProvider(parallel = true)
